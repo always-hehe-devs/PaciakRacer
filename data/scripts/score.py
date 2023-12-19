2140466e709c343b5169e4304e1da6046cf944c7
kami2 <kami.krasowski.uk@gmail.com>
2023-12-19 19:56:02 +0000

    Added util function to render text in fancy way

--- data/scripts/score.py
+++ data/scripts/score.py
@@ -1,16 +1,16 @@
 import pygame
+from data.scripts.util import draw_rotated_text
 
 
 class Score:
     def __init__(self, game):
         self.game = game
         self.total_score = 0
+        self.score_font = pygame.font.SysFont(None, 50)
 
     def add_points(self, points):
         self.total_score += points
 
-    def render_damage(self):
-        pass
-
-    def render_score(self, screen):
-        pass
+    def render_score(self, surface):
+        total_score_position = (100, 100)
+        draw_rotated_text(surface, self.score_font, str(self.total_score), total_score_position, 20, draw_shadow=True)
--- data/scripts/util.py
+++ data/scripts/util.py
@@ -26,3 +26,15 @@ def load_images(path, color_key=None, alpha_convert=None):
     for img_name in sorted(os.listdir(BASE_IMG_PATH + path)):
         images.append(load_image(path + '/' + img_name, color_key=color_key, alpha_convert=alpha_convert))
     return images
+
+
+def draw_rotated_text(surface, font, text, text_position, angle, color="WHITE", draw_shadow=None, shadow_color="BLACK"):
+    if draw_shadow:
+        text_offset = (text_position[0]+4, text_position[1]+1)
+        text_to_draw = font.render(text, True, shadow_color)
+        text_to_draw = pygame.transform.rotate(text_to_draw, angle)
+        surface.blit(text_to_draw, text_offset)
+    text_to_draw = font.render(text, True, color)
+    text_to_draw = pygame.transform.rotate(text_to_draw, angle)
+    surface.blit(text_to_draw, text_position)
+
